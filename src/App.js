6baffbd46b3d99542947c68af96d9d048b58cad1
Jack Lee <38123458+jackhclee@users.noreply.github.com>
2022-04-01 22:37:12 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Logo from "./Logo";
+import BestPokemon from "./BestPokemon";
+import CaughtPokemon from "./CaughtPokemon";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//import React from "react";
+
+// This is the original one
+
+const App = (props) => {
+  // return (<div>
+  //   <header>
+  //     <h1>Welcome to the Pokedex</h1>
+  //     <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"></img>
+  //   </header>
+  // </div>)
+  const abilities = ["Anticipation", "Adaptability", "Run-Away"];
+
+  return (<div id="react-container">
+    <Logo appName="Pokedex" />
+    <BestPokemon abilities={abilities}/>
+    <CaughtPokemon />
+    </div>);
+};
 
 export default App;
